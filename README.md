f0779276ede9fec09a44459164030b97341eca74
Sutan Ervin Lorenza <Sutan@NBMC23040271.local>
2024-03-18 15:09:17 +0700

    adjust script and readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # chatbot-adira
+
+# don't forget to set the open api key
+- export OPENAI_API_KEY='your-api-key'
+
+# how to run
+- streamlit run script.py
--- script.py
+++ script.py
@@ -1,23 +1,34 @@
+import os
+import openai
 import streamlit as st
-from openai import OpenAI
-client = OpenAI()
 
-# Set your OpenAI API key
-OpenAI.api_key = ""
+# Load your OpenAI API key from an environment variable
+api_key = os.getenv('OPENAI_API_KEY')
+if api_key is not None:
+    openai.api_key = api_key
+else:
+    st.error('OpenAI API key not found. Please set it as an environment variable.')
+    st.stop()
+
+# Initialize Streamlit application
+st.title("Adira Chatbot")
 
 # User input field
 user_input = st.text_input("Ask me anything!")
 
 if user_input:
-    completion = client.chat.completions.create( # Change the method name
-        model = 'gpt-3.5-turbo-0125',
-        messages = [ # Change the prompt parameter to messages parameter
-            {"role": "system", "content": "Marv is a factual chatbot that is also sarcastic."},
-            {'role': 'user', 'content': user_input}
-        ],
-        temperature = 0  
-    )
+    # Make a request to the OpenAI API
+    try:
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=[
+                {"role": "system", "content": "You are a helpful tech support chatbot."},
+                {"role": "user", "content": user_input}
+            ]
+        )
+        
+        # Display the chatbot response
+        st.write(response.choices[0].message['content'])
 
-    # Display the chatbot response
-    st.write(completion.choices[0].message.content)
-    st.session_state["user_input"] = ""
+    except openai.OpenAIError as e:
+        st.error(f"An error occurred: {e}")
